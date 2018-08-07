8b405b4ec488e15bf1939cc7a8ebd30256538e6d
jaxxmelbourne27 <41508049+jaxxmelbourne27@users.noreply.github.com>
2018-08-07 15:04:41 +1000

    Update main.cpp

--- _WINDOWS/Source Code/main.cpp
+++ _WINDOWS/Source Code/main.cpp
@@ -1,10 +1,11 @@
 #include "functions.h"
 #include "modules.h"
 bool runState = true;
-main(int argc, char** argv) {
+int main(int argc, char** argv) {
 	functionLibrary Program;
-	cout << "\e[1mopenDATA Build 100\e[0m\nWritten by jaxxmelbourne27\n\nType 'help' for more commands\n\n";
+	cout << "\e[1mopenDATA Build 100\e[0m\n\nType 'help' for more commands\n\n";
 	while(runState = true) {
-		Program.commandline();
+		Program.commandline(runState);
 	}
+	return 0;
 }
